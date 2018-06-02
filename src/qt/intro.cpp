a0dafc1c8c1a63e1858db1bf7c6f8cd471b48474
Francis <francisunderwoodusa@protonmail.com>
2018-06-01 23:18:22 -0700

    Minor GUI tweaks

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -22,10 +22,12 @@
 
 static const uint64_t GB_BYTES = 1000000000LL;
 /* Minimum free space (in GB) needed for data directory */
-static const uint64_t BLOCK_CHAIN_SIZE = 14;
+static const uint64_t BLOCK_CHAIN_SIZE = 1;
 /* Minimum free space (in GB) needed for data directory when pruned; Does not include prune target */
-static const uint64_t CHAIN_STATE_SIZE = 3;
+static const uint64_t CHAIN_STATE_SIZE = 1;
 /* Total required space (in GB) depending on user choice (prune, not prune) */
+static const uint64_t GENESIS_YEAR = 2018;
+/* The year in which the genesis block was created */
 static uint64_t requiredSpace;
 
 /* Check free space asynchronously to prevent hanging the UI thread.
@@ -126,7 +128,7 @@ Intro::Intro(QWidget *parent) :
     ui->lblExplanation1->setText(ui->lblExplanation1->text()
         .arg(tr(PACKAGE_NAME))
         .arg(BLOCK_CHAIN_SIZE)
-        .arg(2009)
+        .arg(GENESIS_YEAR)
         .arg(tr("Bitmoney"))
     );
     ui->lblExplanation2->setText(ui->lblExplanation2->text().arg(tr(PACKAGE_NAME)));
